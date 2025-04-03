0ff7c9fd4a543d4b13e63d84793d28315dbc36fc
Teo <157118568+Teo-dor@users.noreply.github.com>
2025-04-03 11:09:39 +0200

    Qol changes
    After choosing an action the robot will do that action for a very short time to make it easier to train the robot. It will also no longer try to scan again after a scan has been saved because this didn't work. The time has NOT been tested but will be soon to make sure it is reasonable

--- AITraining.py
+++ AITraining.py
@@ -4,6 +4,51 @@ import RPi.GPIO as GPIO
 from rplidar import RPLidar, RPLidarException
 import json
 
+R1 = 22
+R2 = 16
+L1 = 18
+L2 = 13
+def init():    
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(R1, GPIO.OUT)
+    GPIO.setup(R2, GPIO.OUT)
+    GPIO.setup(L1, GPIO.OUT)
+    GPIO.setup(L2, GPIO.OUT)
+
+def forward(sec):
+    init()
+    GPIO.output(R1, False)
+    GPIO.output(R2, True)
+    GPIO.output(L1, True)
+    GPIO.output(L2, False)
+    time.sleep(sec)
+    GPIO.cleanup() 
+def reverse(sec):
+    init()
+    GPIO.output(R1, True)
+    GPIO.output(R2, False)
+    GPIO.output(L1, False)
+    GPIO.output(L2, True)
+    time.sleep(sec)
+    GPIO.cleanup()
+def right_turn(sec):
+    init()
+    GPIO.output(R1, True)
+    GPIO.output(R2, False)
+    GPIO.output(L1, True)
+    GPIO.output(L2, False)
+    time.sleep(sec)
+    GPIO.cleanup()
+def left_turn(sec):
+    init()
+    GPIO.output(R1, False)
+    GPIO.output(R2, True)
+    GPIO.output(L1, False)
+    GPIO.output(L2, True)
+    time.sleep(sec)
+    GPIO.cleanup()
+
+
 # Set the correct serial port for your Lidar
 PORT_NAME = '/dev/ttyUSB0'  # Update based on your system
 motorPin = 11
@@ -29,36 +74,38 @@ def run():
     try:   
         GPIO.output(motorPin, GPIO.HIGH)
         time.sleep(0.5)  # Wait for the motor to reach higher speed
-        while True:
-            print("\n--- Press Ctrl + C to stop the script ---")
-            print("Waiting for a Lidar scan...")
-            recordedScan = None
-            try:
-                for scan in lidar.iter_scans():
-                    recordedScan = scan
-                    #break  # Capture only one scan
-            except KeyboardInterrupt:
-                GPIO.output(motorPin, GPIO.LOW)
-                for measurement in recordedScan:
-                    quality, angle, distance = measurement
-                    print(f"Quality: {quality}, Angle: {angle:.2f}, Distance: {distance:.2f}")
+        print("\n--- Press Ctrl + C to stop the script ---")
+        print("Waiting for a Lidar scan...")
+        recordedScan = None
+        try:
+            for scan in lidar.iter_scans():
+                recordedScan = scan
+                #break  # Capture only one scan
+        except KeyboardInterrupt:
+            GPIO.output(motorPin, GPIO.LOW)
+            for measurement in recordedScan:
+                quality, angle, distance = measurement
+                print(f"Quality: {quality}, Angle: {angle:.2f}, Distance: {distance:.2f}")
 
-                print("\nEnter 'W' for Forward, 'A' for Left, 'D' for Right, 'S' for Brake, or 'Q' to quit:")
-                action = None
-                while action not in ['w', 'a', 'd', 's', 'q']:
-                    action = input("Your action: ").strip().lower()
+            print("\nEnter 'W' for Forward, 'A' for Left, 'D' for Right, 'S' for Brake, or 'Q' to quit:")
+            action = None
+            while action not in ['w', 'a', 'd', 's', 'q']:
+                action = input("Your action: ").strip().lower()
 
-                if action == 'q':
-                    print("Exiting...")
-                    break
+            if action == 'q':
+                print("Exiting...")
 
             action_map = {'w': 0, 'a': 1, 'd': 2, 's': 3}
             print(f"Action selected: {action_map[action]}")
             save_lidar_scan(recordedScan, action_map[action])
             print("Saved!\n")
-            GPIO.output(motorPin, GPIO.HIGH)
-            time.sleep(0.5)  # Restart the motor
-
+            init()
+            if (action == 'w'):
+                forward(0.05)
+            elif (action == 'a'):  
+                left_turn(0.05)    
+            elif (action == 'd'):
+                right_turn(0.05)
     except KeyboardInterrupt:
         print("\nStopping. Data saved.")
     except RPLidarException as e:
@@ -67,6 +114,7 @@ def run():
         GPIO.output(motorPin, GPIO.LOW)
         lidar.stop()
         lidar.disconnect()
+        GPIO.cleanup()
 
 if __name__ == "__main__":
     run()
