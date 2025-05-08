23a8554b2d6ed3b959b359d9267d14976a1f78c0
Teo <157118568+Teo-dor@users.noreply.github.com>
2025-05-08 21:27:46 +0200

    Fixed drifting
    Fixed a problem where the robot would drift to the left using pwm

--- DriveDemo.py
+++ DriveDemo.py
@@ -5,44 +5,55 @@ import tty
 import sys
 import threading
 
-R1, R2, L1, L2 = 22, 16, 18, 13
+R1, R2, L1, L2 = 22, 16, 15, 13
+ENB = 32
 
+right_pwm = 80
 def init():
+    global pwm
     GPIO.setmode(GPIO.BOARD)
     GPIO.setup(R1, GPIO.OUT)
     GPIO.setup(R2, GPIO.OUT)
     GPIO.setup(L1, GPIO.OUT)
     GPIO.setup(L2, GPIO.OUT)
+    GPIO.setup(ENB, GPIO.OUT)
+    pwm = GPIO.PWM(ENB, 1000)
+    pwm.start(0)
 
 def stop():
     GPIO.output(R1, False)
     GPIO.output(R2, False)
     GPIO.output(L1, False)
     GPIO.output(L2, False)
+    pwm.ChangeDutyCycle(0) 
 
 def forward():
     GPIO.output(R1, False)
+    GPIO.output(L2, False)
     GPIO.output(R2, True)
     GPIO.output(L1, True)
-    GPIO.output(L2, False)
+    pwm.ChangeDutyCycle(right_pwm)  
 
 def reverse():
     GPIO.output(R1, True)
     GPIO.output(R2, False)
     GPIO.output(L1, False)
     GPIO.output(L2, True)
+    pwm.ChangeDutyCycle(100)
 
 def right_turn():
     GPIO.output(R1, True)
     GPIO.output(R2, False)
     GPIO.output(L1, True)
     GPIO.output(L2, False)
+    pwm.ChangeDutyCycle(right_pwm) 
 
 def left_turn():
     GPIO.output(R1, False)
     GPIO.output(R2, True)
     GPIO.output(L1, False)
     GPIO.output(L2, True)
+    pwm.ChangeDutyCycle(right_pwm) 
 
 def get_key():
     """Non-blocking key read from terminal."""
@@ -59,7 +70,7 @@ action_map = {'w': forward, 'a': left_turn, 'd': right_turn, 's': reverse}
 
 init()
 try:
-    print("Press W/A/S/D to start moving. Press any other key to stop. Press Q to quit.")
+    print("Press W/A/S/D to move. Hold the key to keep moving. Release the key to stop. Press Q to quit.")
     current_action = None
     stop_thread = False
 
@@ -85,7 +96,7 @@ try:
             current_action = action_map[key]
             current_action()
         else:
-            current_action = None  
+            current_action = None  # Reset current_action when no valid key is pressed
 except KeyboardInterrupt:
     print("\nStopped by Ctrl+C.")
 finally:
